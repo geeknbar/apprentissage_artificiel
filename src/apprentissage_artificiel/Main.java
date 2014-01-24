9a326fa58740e4190fa28043981aad644dd965f3
Thibault Gauthier <thibaultgauthier.etud@gmail.com>
2014-01-24 09:57:30 +0100

    Nouveau fichier de test

--- src/apprentissage_artificiel/Main.java
+++ src/apprentissage_artificiel/Main.java
@@ -3,7 +3,7 @@ package apprentissage_artificiel;
 public class Main {
 
 	public static void main(String[] args) {
-		String filePath = "./bin/doc/weather.nominal.arff";
+		String filePath = "./bin/doc/mika.nominal.arff";
 		Instances instances = new Instances();
 		instances.loadFile(filePath);
 		//instance.displayFile();
