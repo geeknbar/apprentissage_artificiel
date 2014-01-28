0793a0b7b67fe5bf622e324dc7e5e2dde03f4ef5
Thibault Gauthier <thibaultgauthier.etud@gmail.com>
2014-01-28 09:03:52 +0100

    Modification de la structure précédente

--- src/apprentissage_artificiel/ID3.java
+++ src/apprentissage_artificiel/ID3.java
@@ -10,7 +10,7 @@ public class ID3 {
 	public final static String INIT = "";
 
 	private Attribute attribute;
-	private ArrayList<ID3> sons;
+	private HashMap<String, ID3> sons;
 	private ArrayList<String> leafs;
 	
 	public ID3() {
