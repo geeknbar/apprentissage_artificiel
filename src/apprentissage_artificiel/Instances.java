564c3d63006ee19219882c696cb6a8a83e8e775d
Thibault GAUTHIER <gauthierthibault.ac@gmail.com>
2014-02-05 18:30:54 +0100

    Ajout de la fonction compute dans la classe ID3.

--- src/apprentissage_artificiel/Instances.java
+++ src/apprentissage_artificiel/Instances.java
@@ -10,13 +10,11 @@ public class Instances {
 	private String relationName;
 	private LinkedHashMap<String, ArrayList<String>> attributes;
 	ArrayList<Instance> instances;
-	//int numberOfDataRows;
 	
 	public Instances() {
 		this.relationName = INIT;
 		this.attributes = new LinkedHashMap<String, ArrayList<String>>();
 		this.instances = new ArrayList<Instance>();
-		//this.numberOfDataRows = 0;
 	}
 	
 	public void loadFile(String filePath) {
@@ -55,19 +53,7 @@ public class Instances {
 	public void addInstance(Instance instance) {
 		this.instances.add(instance);
 	}
-/*
-	public int getNumberOfDataRows() {
-		return numberOfDataRows;
-	}
 
-	public void setNumberOfDataRows(int numberOfDataRows) {
-		this.numberOfDataRows = numberOfDataRows;
-	}
-	
-	public void incNumberOfDataRows() {
-		this.numberOfDataRows++;
-	}
-	*/
 	public void displayInstances() {
 		System.out.println("Relation Name = " + relationName);
 		
@@ -79,8 +65,5 @@ public class Instances {
 			}
 			System.out.println(instance.getInstanceClass().getValue());
 		}
-		
-		//System.out.println("Number of data rows : " + numberOfDataRows);
-		
 	}
 }
--- src/apprentissage_artificiel/Main.java
+++ src/apprentissage_artificiel/Main.java
@@ -1,22 +1,22 @@
 package apprentissage_artificiel;
 
-import java.util.ArrayList;
-
 public class Main {
 
 	public static void main(String[] args) {
 		//String filePath = "./bin/doc/weather.nominal.arff";
-		String filePath = "./bin/doc/mika.nominal.arff";
+		//String filePath = "./bin/doc/mika.nominal.arff";
+		String filePath = "./bin/doc/tic-tac-toe.arff";
 		Instances instances = new Instances();
 		instances.loadFile(filePath);
 		//instances.displayInstances();
 		ID3 id3 = new ID3();
-		ArrayList<Integer> tmp = new ArrayList<Integer>();
-		tmp.add(0);
-		tmp.add(1);
+		//ArrayList<Integer> tmp = new ArrayList<Integer>();
+		//tmp.add(0);
+		//tmp.add(1);
 		//tmp.add(2);
 		//tmp.add(3);
-		ID3 test = id3.recursive(instances, new InstanceClass("yes"), tmp);
+		//ID3 test = id3.recursive(instances, tmp);
+		ID3 test = id3.compute(instances);
 		test.display(0);
 	}
 
